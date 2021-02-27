f4cc67607850ca97d0ab046a70c141cb9d0b0ef6
Kleiber Maluquish <maluquish.19@gmail.com>
2021-02-26 23:37:05 -0500

    micro servicio entelgy-comments

---
+++ src/main/java/com/entelgy/app/MicroServiceCommentsApplication.java
@@ -0,0 +1,13 @@
+package com.entelgy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceCommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceCommentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entelgy/MicroServiceCommentsServiceTest.java
@@ -0,0 +1,15 @@
+package com.entelgy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class MicroServiceCommentsServiceTest {
+
+	@Test	
+	public void serviceTest() {
+		
+	}
+		
+}
